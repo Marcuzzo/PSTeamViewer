70499b020b4777aac113de82284aeda433070fe6
Marco Micozzi <Marcuzzo@users.noreply.github.com>
2017-09-26 23:03:07 +0200

    updated help build script [skip ci]

--- build/build.help.ps1
+++ build/build.help.ps1
@@ -4,31 +4,61 @@ param(
         Mandatory = $false
     )]
     [ValidateSet('Build', 'Update')]
-    [string] $Mode = 'Build',
+    [string] $Mode = 'Update',
 
     [Parameter()]
     [switch] $Compile
 )
 
-Install-Module -Name platyPS -Scope CurrentUser -Force
+#region Variables
+if(-not $PSScriptRoot)
+{
+    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
 
-Import-Module -Name platyPS
+[string] $Script:HelpDirectory = "$PSScriptRoot/../help"
+[string] $Script:OutputDirectory = "$PSScriptRoot/../PSTeamViewer/en-US/"
+#endregion
 
+#region Init
+
+# Install PlatyPS is not installed
+if ( ! ( Get-Module -Name platyPS -ListAvailable )) 
+{
+    Install-Module -Name platyPS -Scope CurrentUser -Force
+}
+
+# Import the platyPS module
+if ( ! ( Get-Module -Name platyPS ))
+{
+    Import-Module -Name platyPS    
+}
+
+# Import the PSTeamViewer Module
 Import-Module $PSScriptRoot/../PSTeamViewer/PSTeamViewer.psd1 -Force
+#endregion
 
-if ( $Mode -eq 'Build' )
-{
-    New-MarkdownHelp -Module PSTeamViewer -OutputFolder $PSScriptRoot/../help -Force
+
+if ( ! ( Test-Path -Path $Script:HelpDirectory)) {
+    $Mode = 'Build'
 }
-elseif ($Mode -eq 'Update' )
+
+switch( $Mode )
 {
-    Update-MarkdownHelp -Path ..\help
-}
-else
-{
+    
+    'Build' {
+        New-MarkdownHelp -Module PSTeamViewer -OutputFolder $Script:HelpDirectory -Force
+    }
+
+    'Update' {
+        Update-MarkdownHelp -Path $Script:HelpDirectory
+    }
+    
+    Default {}
+
 }
 
 if ( $Compile.IsPresent ) 
 {
-    New-ExternalHelp -Path $PSScriptRoot/../help -OutputPath $PSScriptRoot/../PSTeamViewer/en-US/ -Force
+    New-ExternalHelp -Path $Script:HelpDirectory -OutputPath $Script:OutputDirectory -Force
 }
