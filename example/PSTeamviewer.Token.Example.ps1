c0708e5e7a77b24c10c7559e7a0426c17825bb89
Marco Micozzi <Marcuzzo@users.noreply.github.com>
2017-09-27 07:58:31 +0200

    updated examples [skip ci]

---
+++ example/PSTeamviewer.Token.Example.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param()
+
+
+Import-Module "$PSScriptRoot\..\PSTeamViewer\PSTeamViewer.psd1"
+
+# Store your token in an environment variable 
+[string] $Token = $env:TVAccessToken
+
+# Initialize the API
+Initialize-TVAPI -Token $Token 
+
+# Get the Active Directory users based 
+Get-TVUser | ForEach-Object {
+
+    # This obviously only works if the names in 
+    # the TeamViewer management console is correct
+    Get-ADUser -Filter {Name -like "*$_*"} | Select-Object -Property Name, UserPrincipalName
+
+}
+
