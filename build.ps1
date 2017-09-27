e529a4c2b84c736dd63ff72edfc43935e6e393a8
Marco Micozzi <Marcuzzo@users.noreply.github.com>
2017-09-27 21:42:42 +0200

    updated build scripts for appveyor

--- build.ps1
+++ build.ps1
@@ -1,52 +1,33 @@
-function Resolve-Module
-{
-    [Cmdletbinding()]
-    param
-    (
-        [Parameter(Mandatory)]
-        [string[]]$Name
-    )
-    Process
-    {
-        foreach ($ModuleName in $Name)
-        {
-            $Module = Get-Module -Name $ModuleName -ListAvailable
-            Write-Verbose -Message "Resolving Module $($ModuleName)"
 
-            if ($Module)
-            {
-                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-
-                if ($Version -lt $GalleryVersion)
-                {
-                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
-
-                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
-                    Install-Module -Name $ModuleName -Force -SkipPublisherCheck
-                    Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
-                }
-                else
-                {
-                    Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
-                    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-                }
-            }
-            else
-            {
-                Write-Verbose -Message "$($ModuleName) Missing, installing Module"
-                Install-Module -Name $ModuleName -Force -SkipPublisherCheck
-                Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-            }
-        }
-    }
-}
-
-# Grab nuget bits, install modules, set build variables, start build.
+#----------------------------------------------------------#
+#    Nuget
+#----------------------------------------------------------#
+Write-Host -Object 'Installing Nuget'
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module Psake, Pester, PSScriptAnalyzer, platyPS -Verbose
 
+#----------------------------------------------------------#
+# Install Modules
+#----------------------------------------------------------#
+
+Write-Host -Object 'Starting the installation of modules...'
+
+'psake', 'Pester', 'PSScriptAnalyzer', 'platyPS' | ForEach-Object {
+
+    $ModuleName = $_
+
+    Write-Host -Object ('Installing module "{0}".' -f $ModuleName)
+    Install-Module -Name $ModuleName -Repository PSGallery -Force
+
+    Write-Host -Object ('Importing module: "{0}".' -f $ModuleName)
+    Import-Module -Name $ModuleName
+
+}
+Write-Host -Object 'Done installing modules.'
+
+
+#----------------------------------------------------------#
+#----------------------------------------------------------#
 
 Invoke-psake .\psake.ps1
 exit ( [int]( -not $psake.build_success ) )
