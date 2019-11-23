7f6788786adaa5910cc677cfebe002b57c8a131c
Marco Micozzi <marco.micozzi@gmail.com>
2019-11-23 21:13:10 +0100

    Modified Type of LastSeen from String to DateTime
    Also added a check to add a null when the json data returned from the
    web api is not present or empty.

--- PSTeamViewer/public/Get-TVDevice.ps1
+++ PSTeamViewer/public/Get-TVDevice.ps1
@@ -61,6 +61,16 @@ function Get-TVDevice
 
             Write-Verbose -Message $_
 
+            if ($null -eq $_.last_seen)
+            {
+                $LastSeen = $null
+            }
+            else
+            {
+                $LastSeen = Get-Date -Date $_.last_seen
+            }
+
+            Write-Verbose -Message ('Last Seen: {0}' -f $LastSeen)
             [TVDevice] $TVDevice = New-Object -TypeName TVDevice -Property @{
                 RemoteControlID   = $_.remotecontrol_id
                 DeviceID          = $_.device_id
@@ -69,7 +79,7 @@ function Get-TVDevice
                 OnlineStatus      = $_.online_state
                 AssignedTo        = $_.assigned_to
                 SupportedFeatures = $_.supported_features
-                LastSeen          = $_.last_seen
+                LastSeen          = $LastSeen
                 PolicyID          = $_.policy_id
                 Description       = $_.description
             }
