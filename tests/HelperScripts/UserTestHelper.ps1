578cd67a4ebcdd3849cbcc21424286e8acd92159
Marco Micozzi <marco.micozzi@gmail.com>
2019-11-25 00:42:40 +0100

    Added some TVUser tests
    - Added helper script for TVUser testing.
    - Added checks for Get-TVUSer.

---
+++ tests/HelperScripts/UserTestHelper.ps1
@@ -0,0 +1,199 @@
+
+# Set-Content -Path $JsonDataFile -Value '{ "users" : [  { "id" : "u0000001",    "name" : "Mighty Administrator",    "permissions" : "ManageAdmins, ManageUsers, ShareOwnGroups, EditFullProfile, ViewAllConnections, ViewOwnConnections, EditConnections, DeleteConnections, ManagePolicies, AssignPolicies, AcknowledgeAllAlerts,AcknowledgeOwnAlerts, ViewAllAssets, ViewOwnAssets, EditAllCustomModuleConfigs, EditOwnCustomModuleConfigs",    "active": true,    "email": "admin@example.com"  },  { "id" : "u0000002",    "name" : "John Doe",    "permissions" : "EditFullProfile",    "email": "user@example.com"  }] } '
+
+
+
+function Initialize-UserData
+{
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string] $Path
+    )
+
+    $DefaultTestJsonData = @{
+        Users = @(
+            @{
+                id                     = ("u{0:0000000}" -f 1)
+                name                   = 'Mighty Administrator'
+                email                  = 'admin@example.com'
+                permissions            = 'ManageAdmins, ManageUsers, ShareOwnGroups, EditFullProfile, ViewAllConnections, ViewOwnConnections, EditConnections, DeleteConnections, ManagePolicies, AssignPolicies, AcknowledgeAllAlerts,AcknowledgeOwnAlerts, ViewAllAssets, ViewOwnAssets, EditAllCustomModuleConfigs, EditOwnCustomModuleConfigs'
+                active                 = $true
+                custom_quicksupport_id = 'auto'
+                custom_quickjoin_id    = 'auto'
+            }
+        )
+    }
+    $DefaultTestJsonData | ConvertTo-Json | Out-File $Path
+    #Add-UserData -Path $Path -Name 'Mighty Administrator' -Email 'admin@example.com'
+}
+
+function Get-NewUserID
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory = $true
+        )]
+        [string] $Path
+    )
+    $Json = Get-Content -Path $Path -Raw | ConvertFrom-Json
+    $ID = ($Json.users | Sort-Object -Property id | Select-Object -Last 1 -Property id).id
+    if ( $null -eq $ID )
+    {
+        [int] $newID = 0
+    }
+    else
+    {
+        [int] $newID = [int]($ID -replace 'u', '')
+    }
+    return "u{0:0000000}" -f ($newID + 1)
+}
+
+function Add-UserData
+{
+    [CmdletBinding()]
+    param(
+
+        [Parameter(
+            Mandatory = $true
+        )]
+        [string] $Path,
+
+        [Parameter(
+            Mandatory = $true
+        )]
+        [string] $Name,
+
+        [Parameter(
+            Mandatory = $true
+        )]
+        [string] $Email,
+
+
+        [Parameter()]
+        [string] $Language = 'en',
+
+        [Parameter(
+            Mandatory = $false
+        )]
+        [string] $Permissions = 'ShareOwnGroups, ViewOwnConnections, EditConnections, EditFullProfile',
+
+        [Parameter()]
+        [string] $QuickSupportID = 'auto',
+
+        [Parameter()]
+        [string] $QuickJoinID = 'auto'
+
+    )
+
+    $ID = Get-NewUserID -Path $Path
+
+    $UserData = @{
+        id                     = $ID
+        name                   = $Name
+        permissions            = $Permissions
+        active                 = $true
+        email                  = $Email
+        custom_quicksupport_id = $QuickSupportID
+        custom_quickjoin_id    = $QuickJoinID
+    }
+    $Obj = Get-UserData -JsonDataFile $Path
+    $obj.users += $UserData
+    $obj | ConvertTo-Json | Out-File $Path
+}
+
+function Get-UserData
+{
+    [CmdletBinding(
+        DefaultParameterSetName = 'none'
+    )]
+    param(
+
+        [Parameter(
+            Mandatory = $true
+        )]
+        [string] $JsonDataFile,
+
+        [Parameter(
+            Mandatory = $false,
+            ParameterSetName = 'ByID'
+        )]
+        [string] $ID,
+
+        [Parameter(
+            Mandatory = $false,
+            ParameterSetName = 'ByName'
+        )]
+        [string] $Name,
+
+        [Parameter(
+            Mandatory = $false,
+            ParameterSetName = 'ByEmail'
+        )]
+        [string] $Email
+
+    )
+
+    Write-Verbose -Message 'Reading json data'
+
+    if ( Test-Path -Path $JsonDataFile )
+    {
+
+        $JsonFileContent = Get-Content -Path $JsonDataFile -Raw
+        Write-Verbose -Message ('JSON Content: {0}' -f $JsonFileContent)
+        $Json = $JsonFileContent | ConvertFrom-Json
+
+        $UserJson = @{
+            users = @()
+        }
+        Write-Verbose -Message 'Selecting output...'
+        switch ($PSCmdlet.ParameterSetName)
+        {
+            'ByID'
+            {
+                Write-Verbose -Message ('Returning json for ID: {0}' -f $ID)
+                $Json.users | where { $_.id -eq $ID }
+            }
+            'ByName'
+            {
+                $data = $Json.users | where { $_.name -like "*$Name*" }
+                $UserJson.users += $data
+                Write-Output -InputObject $UserJson
+            }
+            'ByEmail'
+            {
+                $data = $Json.users | where { $_.email -like "*$Email*" }
+                $UserJson.users += $data
+                Write-Output -InputObject $UserJson
+            }
+            default
+            {
+                $Json
+            }
+        }
+    }
+    else
+    {
+        Write-Warning -Message ('The file: "{0}" was not found' -f $JsonDataFile)
+    }
+}
+
+function Set-UserData
+{
+    [CmdletBinding()]
+    param()
+}
+function Remove-UserData
+{
+    [CmdletBinding()]
+    param()
+}
+
+# Add-UserData -Email 'test@example.com' -Name 'testuser' -Pwd 'testPWD'
+# Add-UserData -Email 'test2@example.com' -Name 'testuser2' -Pwd 'testPWD'
+
+
+# Cleaning up
+#Remove-Item -Path $JsonDataFile
+
---
+++ tests/TVUser.tests.ps1
@@ -0,0 +1,114 @@
+# Import the PSTeamViewer Module
+Import-Module "$PSScriptRoot\..\PSTeamViewer\PSTeamViewer.psd1" -Force
+
+. $PSScriptRoot\HelperScripts\UserTestHelper.ps1
+
+# This token will not actually be used as the Invoke-RestMethod is mocked.
+[string] $TVAPIToken = 'ABC12345'
+
+# Limit the scope to the PSTeamViewer Module
+InModuleScope PSTeamViewer {
+
+    Describe "TVUser Tests" {
+
+        # The path to the temp jsson file
+        $DataFile = 'TestDrive:\users.json'
+
+        Initialize-UserData -Path $DataFile
+
+        Mock -CommandName Invoke-RestMethod -Verifiable -MockWith {
+
+            Write-Verbose -Message ('Got URL: {0}' -f $Uri)
+            Write-Verbose -Message ('Got Body: {0}' -f $Body)
+
+            if ( $Uri -match '/users/(.+)$')
+            {
+                Write-Verbose -Message ('Got request for user with ID: {0}' -f $Matches[1])
+                $Data = Get-UserData -JsonDataFile $DataFile -ID $Matches[1]
+            }
+            else
+            {
+                Write-Verbose -Message ('GOT URI: {0}' -f $Uri)
+                if (-not ( [string]::IsNullOrEmpty($Body.name)) )
+                {
+                    $Data = Get-UserData -JsonDataFile $DataFile -Name $Body.name
+                }
+                elseif (-not ( [string]::IsNullOrEmpty($Body.email)))
+                {
+                    $Data = Get-UserData -JsonDataFile $DataFile -Email $Body.email
+                }
+            }
+            Write-Verbose -Message ('returned data: {0}' -f $Data)
+
+            return $Data
+
+        } -ParameterFilter {
+            $uri -match "/api/v1/users" -and $Method -eq 'Get'
+        } -ModuleName PSTeamViewer
+
+
+        Context 'Getting User by UserID' {
+
+            $TVUser = Get-TVUser -UserID 'u0000001' -Token $TVAPIToken
+
+            It 'Tests the type of returned object' {
+                $TVUser.GetType().Name | should be 'TVUser'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+            It 'Tests the user ID' {
+                $TVUser.ID | Should be 'u0000001'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+            It 'Tests the name of the user' {
+                $TVUser.Name | should be 'Mighty Administrator'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+        }
+
+        Context 'Getting User by email' {
+
+            $TVUser = Get-TVUser -Token $TVAPIToken -Email 'admin@example.com'
+
+            It 'Tests the type of returned object' {
+                $TVUser.GetType().Name | should be 'TVUser'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+            It 'Tests the user ID' {
+                $TVUser.ID | Should be 'u0000001'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+            It 'Tests the name of the user' {
+                $TVUser.Name | should be 'Mighty Administrator'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+        }
+
+        Context 'Getting User by name' {
+
+            $TVUser = Get-TVUser -Token $TVAPIToken -Name 'Mighty Administrator'
+
+            It 'Tests the type of returned object' {
+                $TVUser.GetType().Name | should be 'TVUser'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+            It 'Tests the user ID' {
+                $TVUser.ID | Should be 'u0000001'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+            It 'Tests the name of the user' {
+                $TVUser.Name | should be 'Mighty Administrator'
+                Assert-MockCalled -CommandName Invoke-RestMethod -ModuleName PSTeamviewer
+            }
+
+        }
+
+    }
+}
