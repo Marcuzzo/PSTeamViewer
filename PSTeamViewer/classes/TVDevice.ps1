58311f1cf391f43ffccbba2c1b7c4b94cb555b40
Marco Micozzi <marco.micozzi@gmail.com>
2019-11-29 09:20:00 +0100

    Added documentation to the TVDevice class

--- PSTeamViewer/classes/TVDevice.ps1
+++ PSTeamViewer/classes/TVDevice.ps1
@@ -1,19 +1,81 @@
-﻿Class TVDevice
+﻿<#
+    TVDevice Class.
+    This device represents the json data as returned by the Teamviewer API documentation (Version 13.2.0).
+    See section 'Devices' on page 51 for more information.
+#>
+Class TVDevice
 {
-    [string] $RemoteControlID
+
+    #region Fields
+
+    <#
+        The ID that is unique for this entry of the computers & contacts list.
+        Values are always prefixed with a ‘d’.
+    #>
     [string] $DeviceID
-    [string] $Alias
+
+    <#
+        The ID that is unique to this device and can be used to start a remote control session.
+    #>
+    [string] $RemoteControlID
+
+    <#
+        The ID of the group that this device is a member of.
+    #>
     [string] $GroupID
-    [string] $OnlineStatus
-    [bool] $AssignedTo
-    [string] $SupportedFeatures
+
+    <#
+        The alias that the current user has given to this device.
+    #>
+    [string] $Alias
+
+    <#
+        The description that the current user has entered for this device.
+    #>
     [string] $Description
+
+    <#
+        The current online state of the device.
+        * Possible values are: online, offline.
+    #>
+    [string] $OnlineStatus
+
+    <#
+        The features supported by the device.
+        * Possible values are: chat, remote_control.
+    #>
+    [string] $SupportedFeatures
+
+    <#
+        Indicates whether the device is assigned to the current user.
+        * Possible values are: true, false.
+    #>
+    [bool] $AssignedTo
+
+    <#
+        ID of the policy that is assigned to the device.
+        * Possible values are: a policy_id, inherit.
+        License restrictions apply, see chapter Licensing.
+    #>
     [string] $PolicyID
+
+    <#
+        The timestamp of the last time, the device was online.
+        Is not returned if the device is currently online.
+        Only available if the device is assigned to the current user.
+    #>
     [Nullable[DateTime]] $LastSeen
 
-    # Dummy constructor
+    #endregion
+
+    <#
+        Default constructor
+    #>
     TVDevice() { }
 
+    <#
+        Constructor with arguments
+    #>
     TVDevice ( [string] $RemoteControlID,
         [string] $DeviceID,
         [string] $Alias,
