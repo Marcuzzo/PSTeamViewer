67c9545db2419951a7b0aba90e4c4c092313eb61
Marco Micozzi <marco.micozzi@gmail.com>
2020-01-23 22:13:30 +0100

    indentation cleanup

--- PSTeamViewer/public/Get-TVAccount.ps1
+++ PSTeamViewer/public/Get-TVAccount.ps1
@@ -15,12 +15,12 @@ function Get-TVAccount
     {
 
         $Response = Invoke-TVApiRequest -Token $Token -Resource account -Method GET -Verbose
-        
+
         # userid needs to be renamed to ID because the TVPrincipal class uses ID and not UserID
         $json = ($Response | ConvertTo-Json) -replace 'userid', 'id'
-                               
+
         Write-Output -InputObject ([TVAccount]( $json | ConvertFrom-Json ) )
-       
+
     }
 
 }
